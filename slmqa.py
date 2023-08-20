bdbcf57fecbea48a078b9914be8ea54c4579b4f0
Alasdair <77910352+alasdairforsythe@users.noreply.github.com>
2023-08-20 15:28:11 +0700

    Add files via upload

--- slmqa.py
+++ slmqa.py
@@ -4,8 +4,8 @@ class Benchmark:
     def __init__(self, filename):
         with open(filename, 'r') as f:
             self.data = json.load(f)
-        self.score = 0
-        self.total = 0
+        self._score = 0
+        self._total = 0
 
     def questions(self):
         return [item['question'] for item in self.data]
@@ -13,13 +13,13 @@ class Benchmark:
     def submit_answer(self, idx, answer):
         answer = answer.lower()
         correct_answer = self.data[idx]['answer']
-        self.total += 1
+        self._total += 1
         if correct_answer in answer:
-            self.score += 1
+            self._score += 1
             return True
         return False
 
     def score(self):
-        if self.total == 0:
+        if self._total == 0:
             return 0
-        return (self.score * 100) / self.total
+        return (self._score * 100) / self._total
